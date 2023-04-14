db9b0f5ae5b4284d097617ae2c7c19a959043a9b
Goran Petkovic <88789363+GoranUI@users.noreply.github.com>
2023-04-14 22:24:04 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -26,20 +26,78 @@ const initFirebase = async () => {
 };
 
 const saveButton = document.getElementById('btnSave');
+
+
 const titleField = document.getElementById('title');
 const aboutField = document.getElementById('about');
-const linkField = document.getElementById('Link');
-const tagField = document.getElementById('tag');
-const imgLinkField = document.getElementById('imgLink');
+
+const twitterUsernameField = document.getElementById('twitterUsername');
+const twitterURLField = document.getElementById('twitterURL');
+const twitterImgField = document.getElementById('twitterImg');
+const instagramUrlField = document.getElementById('instagramUrl');
+const websiteUrlField = document.getElementById('websiteUrl');
+
+const slugField = document.getElementById('slug');
+const tagField = document.getElementById('filter-tag');
+
+const gearItem1Field = document.getElementById('gear1');
+const gearItem2Field = document.getElementById('gear2');
+const gearItem3Field = document.getElementById('gear3');
+const gearItem4Field = document.getElementById('gear4');
+const gearItem5Field = document.getElementById('gear5');
+
+const imgLink1Field = document.getElementById('imgLink1');
+const imgLink2Field = document.getElementById('imgLink2');
+const imgLink3Field = document.getElementById('imgLink3');
+const imgLink4Field = document.getElementById('imgLink4');
+const imgLink5Field = document.getElementById('imgLink5');
+const imgLink6Field = document.getElementById('imgLink6');
+const imgLink7Field = document.getElementById('imgLink7');
+const imgLink8Field = document.getElementById('imgLink8');
+const imgLink9Field = document.getElementById('imgLink9');
+const imgLink10Field = document.getElementById('imgLink10');
+const imgLink11Field = document.getElementById('imgLink11');
+const imgLink12Field = document.getElementById('imgLink12');
+
+
+
+
 
 saveButton.addEventListener('click', (event) => {
   event.preventDefault();
 
   const titleValue = titleField.value;
   const aboutValue = aboutField.value;
-  const linkValue = linkField.value;
+
   const tagValue = tagField.value;
-  const imgLinkValue = imgLinkField.value;
+  const slugValue = slugField.value;
+
+  const twitterUsernameValue =  twitterUsernameField.value;
+  const twitterURLValue =  twitterURLField.value;
+  const twitterImgValue =  twitterImgField.value;
+  const instagramUrlValue =  instagramUrlField.value;
+  const websiteUrlValue =  websiteUrlField.value;
+
+  const gear1Value = gearItem1Field.value;
+  const gear2Value = gearItem2Field.value;
+  const gear3Value = gearItem3Field.value;
+  const gear4Value = gearItem4Field.value;
+  const gear5Value = gearItem5Field.value;
+
+
+  const imgLink1Value = imgLink1Field.value;
+  const imgLink2Value = imgLink2Field.value;
+  const imgLink3Value = imgLink3Field.value;
+  const imgLink4Value = imgLink4Field.value;
+  const imgLink5Value = imgLink5Field.value;
+  const imgLink6Value = imgLink6Field.value;
+  const imgLink7Value = imgLink7Field.value;
+  const imgLink8Value = imgLink8Field.value;
+  const imgLink9Value = imgLink9Field.value;
+  const imgLink10Value = imgLink10Field.value;
+  const imgLink11Value = imgLink11Field.value;
+  const imgLink12Value = imgLink12Field.value;
+
 
   const collectionValue = document.getElementById('collections').value;
 
@@ -48,30 +106,88 @@ saveButton.addEventListener('click', (event) => {
   push(ref(db, collectionValue), {
     title: titleValue,
     about: aboutValue,
-    link: linkValue,
+
+    slug: slugValue,
     tag: tagValue,
-    imgLink: imgLinkValue,
+
+    img1Link: imgLink1Value,
+    twitterUsername: twitterUsernameValue,
+    twitterURL: twitterURLValue,
+    twitterImg: twitterImgValue,
+    instagramUrl: instagramUrlValue,
+    websiteURL: websiteUrlValue,
+
+    gear1: gear1Value,
+    gear2: gear2Value,
+    gear3: gear3Value,
+    gear4: gear4Value,
+    gear5: gear5Value,
+
+    image1: imgLink1Value,
+    image2: imgLink2Value,
+    image3: imgLink3Value,
+    image4: imgLink4Value,
+    image5: imgLink5Value,
+    image6: imgLink6Value,
+    image7: imgLink7Value,
+    image8: imgLink8Value,
+    image9: imgLink9Value,
+    image10: imgLink10Value,
+    image11: imgLink11Value,
+    image12: imgLink12Value,
+
+
 
   }).then(() => {
     titleField.value = '';
     aboutField.value = '';
-    linkField.value = '';
+
+    slugField.value = '';
     tagField.value = '';
-    imgLinkField.value = '';
+
+    twitterUsernameField.value = '';
+    twitterImgField.value = '';
+    twitterURLField.value = '';
+    instagramUrlField.value = '';
+    websiteUrlField.value = '';
+
+    gearItem1Field.value = '';
+    gearItem2Field.value = '';
+    gearItem3Field.value = '';
+    gearItem4Field.value = '';
+    gearItem5Field.value = '';
+
+    imgLink1Field.value = '';
+    imgLink2Field.value = '';
+    imgLink3Field.value = '';
+    imgLink4Field.value = '';
+    imgLink5Field.value = '';
+    imgLink6Field.value = '';
+    imgLink7Field.value = '';
+    imgLink8Field.value = '';
+    imgLink9Field.value = '';
+    imgLink10Field.value = '';
+    imgLink11Field.value = '';
+    imgLink12Field.value = '';
+    
   }).catch((error) => {
     console.error(error);
   });
+
+  
 });
 
 
 
 function loadPosts() {
   const { db, ref, onValue } = firebase;
+  const collections = ['workspaces', 'user-interfaces' ,'branding', 'dopefolios'];
+  const postsContainer = document.getElementById('posts-container');
+  postsContainer.innerHTML = '';
 
-
+for (const collectionValue of collections) {
   onValue(ref(db, collectionValue), (snapshot) => {
-    const postsContainer = document.getElementById('posts-container');
-    postsContainer.innerHTML = '';
+    
 
     snapshot.forEach((childSnapshot) => {
       const post = childSnapshot.val();
@@ -96,7 +212,6 @@ function loadPosts() {
       postImg.classList.add('card-img');
       postImg.src = post.imgLink;
 
-
       const postTag = document.createElement('div');
       postTag.classList.add('post-tag');
       postTag.textContent = post.tag;
@@ -110,6 +225,8 @@ function loadPosts() {
       postsContainer.appendChild(postCard);
     });
   });
+}
+ 
 }
 
 window.onload = async () => {
---
+++ js/style.js
@@ -0,0 +1,91 @@
+// 
+//  Image Container Expand Script
+const showMoreBtn = document.querySelector('#show-more-btn');
+const showLessBtn = document.querySelector('#show-less-btn');
+
+showMoreBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const container = document.querySelector('.container-big');
+    container.style.maxHeight = 'none';
+    showMoreBtn.style.display = 'none';
+    showLessBtn.style.display = 'block';
+});
+
+showLessBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    a
+    const container = document.querySelector('.container-big');
+    container.style.maxHeight = '748px';
+    showMoreBtn.style.display = 'block';
+    showLessBtn.style.display = 'none';
+});
+
+
+// 
+//  Modal Script
+
+const modal = document.getElementById('modal');
+const btnModal = document.getElementById('btnModal');
+const btnSave = document.getElementById('btnSave');
+const backBtn = document.getElementById('backBtn');
+
+btnModal.addEventListener('click', () => {
+    modal.style.display = 'flex';
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal || event.target === backBtn) {
+    modal.style.display = 'none';
+    }
+});
+
+// 
+// Random Message Script
+
+const messages = [
+    "From brother Goran, with love galore,\nEnjoy using our data tool, forevermore.",
+    "Data tool in hand, you're ready to rule,\nThanks to brother Goran, you're never a fool.",
+    "Here's a tool to use like a pro,\nFrom brother Goran, with love in tow.",
+    "Our data tool's the best in the land,\nThanks to brother Goran's helping hand.",
+    "Use our data tool, it's oh so cool,\nThanks to brother Goran, it's your secret tool.",
+    "Brother Goran's Data tool  \nto keep this project cool.",
+    "Safe Data, nothing to betray, \nbrother Goran sending love your way.",
+    "From Brother Goran with love, \nthis Data Tool is sent from above."
+];
+
+
+const logosub = document.querySelector('.logosub');
+logosub.innerText = `- ${messages[Math.floor(Math.random() * messages.length)]}`;
+
+
+// 
+
+// Twitte Fields Script
+
+const twitterUsername = document.querySelector('#twitterUsername');
+const twitterURL = document.querySelector('#twitterURL');
+
+twitterUsername.addEventListener('input', () => {
+  // Remove "@" from the Twitter username
+  const username = twitterUsername.value.replace('@', '');
+
+  
+  // Set the value of the Twitter URL field
+  twitterURL.value = `https://twitter.com/${username}`;
+});
+
+const saveButton = document.getElementById('btnSave');
+const textStack = document.querySelector('.text-stack');
+const buttonStack = document.querySelector('.button-stack');
+
+saveButton.addEventListener('click', () => {
+  // perform action to hide button-stack
+  buttonStack.style.display = 'none';
+  
+  // perform action to display checkmark animation
+  textStack.innerHTML = `
+    <h2>Congratulations!</h2>
+    <p>Your form has been submitted successfully &#x2714;</p>
+  `;
+});
